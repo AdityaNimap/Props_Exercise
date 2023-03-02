a781d332bc0d8dd36da3d6c32d7b0f03ecb66903
Aditya Titame <adityatitame@nimapinfotech.com>
2023-03-02 15:22:41 +0530

    Completed Props exercise

---
+++ src/Components/A.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import B from './B'
+const A = () => {
+  return (
+    <div>
+      <B name="Aditya" company="Nimap Infotech"/>
+    </div>
+  )
+}
+
+export default A
---
+++ src/Components/B.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const B = (props) => {
+  return (
+    <div>
+      <h3>My name is {props.name} and working in {props.company}</h3>
+    </div>
+  )
+}
+
+export default B
